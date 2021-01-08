41a81c9081deaaabdf389133c42c2a4027de562e
Greg Hull <greg@cjrc.us>
2021-01-08 10:54:45 -0500

    just working on it

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# meerkat
+# This is the main title
+
+## This is a subsection
+
+### Third seciton
+
+This is a paragraph about things.  Here is a lot of typing.  More information
+about Jane [is available here][1].
+
+A list of fact:
+
+1) Hello
+2) Charles
+3) Another thing
+
+[1]:https://www.janegoodall.org/
+
+<div class="getty embed image" style="background-color:#fff;display:inline-block;font-family:Roboto,sans-serif;color:#a7a7a7;font-size:11px;width:100%;max-width:594px;"><div style="padding:0;margin:0;text-align:left;"><a href="http://www.gettyimages.com/detail/970466758" target="_blank" style="color:#a7a7a7;text-decoration:none;font-weight:normal !important;border:none;display:inline-block;">Embed from Getty Images</a></div><div style="overflow:hidden;position:relative;height:0;padding:66.66667% 0 0 0;width:100%;"><iframe src="//embed.gettyimages.com/embed/970466758?et=RxwsGnlPS1pY9X98ZIut7A&tld=com&sig=Nu6WMwa0UwmM0RkK-LhlENBooSzF4BBMTbh96Qzxf54=&caption=true&ver=1" scrolling="no" frameborder="0" width="594" height="396" style="display:inline-block;position:absolute;top:0;left:0;width:100%;height:100%;margin:0;"></iframe></div></div>
--- meerkat.go
+++ meerkat.go
@@ -1,4 +1,92 @@
 package main
 
+import (
+	"bytes"
+	"flag"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
+)
+
+var (
+	addr = flag.String("addr", "0.0.0.0:8080", "Address for listening")
+	root = flag.String("root", ".", "Root directory for serving files")
+
+	MarkdownSuffix = regexp.MustCompile(`(?i).*\.md$`)
+	Markdown       = goldmark.New(
+		goldmark.WithExtensions(extension.GFM, extension.Typographer),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithUnsafe(),
+		),
+	)
+
+	htmlText = `
+<html>
+<header>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</header>
+<body>
+<div class="container">
+{{.}}
+</div>
+</body>
+</html>
+`
+	Template = template.Must(template.New("html").Parse(htmlText))
+)
+
+func markdownHandler(w http.ResponseWriter, r *http.Request) {
+	source, err := ioutil.ReadFile(*root + r.URL.Path)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	var buf bytes.Buffer
+	if err := Markdown.Convert(source, &buf); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	if err := Template.Execute(w, template.HTML(buf.Bytes())); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+	}
+
+}
+
+func router(w http.ResponseWriter, r *http.Request) {
+	if strings.HasSuffix(r.URL.Path, "/") {
+		r.URL.Path += "index.md"
+	}
+
+	if MarkdownSuffix.MatchString(r.URL.Path) {
+		markdownHandler(w, r)
+	} else {
+		http.FileServer(http.Dir(*root)).ServeHTTP(w, r)
+	}
+}
+
 func main() {
+	flag.Parse()
+	if *root == "" {
+		*root = "."
+	}
+
+	http.HandleFunc("/", router)
+
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
